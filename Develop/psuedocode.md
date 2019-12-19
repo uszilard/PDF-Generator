6542f396a859c4ffebad154f9d09f68af42b3a8b
Szilard Udroiu <uszilard@gmail.com>
2019-12-19 18:49:22 +0100

    fixes

--- Develop/psuedocode.md
+++
@@ -1,7 +0,0 @@
-# Ask the user for GitHub username
-# Ask the user for a favorite color
-# Get the user input (username, color)
-# Get Github Data (From the API)
-# Pick the right color form the available theme colors
-# Generate PDF depends on the generated HTML
-# Save to a resume.pdf
--- index.js
+++ index.js
@@ -1,40 +1,74 @@
-var inquirer = require('inquirer');
-var axios = require('axios');
-var html_pdf = require('html-pdf');
+const inquirer = require("inquirer");
+const Octokit = require("@octokit/rest");
+const generator = require("./generateHTML.js");
+const puppeteer = require("puppeteer");
+const path = require("path");
 
+async function getGitHubUser(answers) {
+    // https://octokit.github.io/rest.js/#usage
+    // User agent is required
+    const octokit = Octokit({
+        userAgent: "portfolio-generator v0.1"
+    });
 
-
-const questions = [
-
-];
-
-function writeToFile(fileName, data) {
-
+    try {
+        let {
+            data
+        } = await octokit.users.getByUsername({
+            username: answers.user
+        });
+        console.log(`Fetched GitHub data for user ${data.login}`);
+        return data;
+    } catch (error) {
+        console.log("Could not fetch GitHub data");
+    }
 }
 
-function init() {
-    inquirer.prompt([
-        {
+async function printPDF(html) {
+    try {
+        const browser = await puppeteer.launch({
+            // these arguments allow puppeteer to work on WSL 2 without going through quite a bit of work to set up a viable sandbox
+            // see https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox for more info
+            // since we trust the HTML (we create it ourselves!) this should be fine
+            args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        });
+        const page = await browser.newPage();
+        await page.setContent(html, {
+            waitUntil: "networkidle0"
+        });
+        await page.pdf({
+            path: path.join(__dirname, "profile.pdf"),
+            format: "A4"
+        });
+        await browser.close();
+        console.log("PDF generated!");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+(async function init() {
+    const questions = [{
             type: "input",
-            name: "githubUsername",
-            message: "What is your Github username?",
-
+            name: "user",
+            message: "Enter a GitHub username:"
+        },
+        {
+            type: "list",
+            name: "color",
+            message: "Pick a color theme:",
+            choices: ["red", "blue", "pink", "green"]
         }
-    ]).then(answers => {
-
-        var githubUsername = answers.githubUsername
-
-        axios.get(`https://api.github.com/users/${githubUsername}`)
-            .then(function (response) {
-                // handle success
-                console.log(response.data.bio);
-                console.log(response.data.public_repos);
-                console.log(response.data.followers);
-                console.log(response.data.following);
-
-
-            })
-    })
-}
-init();
-
+    ];
+    try {
+        let answers = await inquirer.prompt(questions);
+        let data = await getGitHubUser(answers);
+        let html = generator.generateHTML({
+            color: answers.color,
+            user: data
+        });
+        await printPDF(html);
+    } catch (error) {
+        console.log(error);
+    }
+})();
